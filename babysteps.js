1b8ba6b08d95de5bad7dcfd9f6beb41bf1be8a2d
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-04-19 13:42:17 -0400

    fixed exercise two

--- babysteps.js
+++ babysteps.js
@@ -1,7 +1,9 @@
+var arr = process.argv;
 function babysteps(array){
 	var summation = 0;
-	for (var i = 2; i < process.argv.length; i++){
-		summation += +process.argv[i];
+	for (var i = 2; i < array.length; i++){
+		num = +array[i];
+		summation += num;
 	}
 	console.log(summation);
 }
